f75cc8e02174e7536714a3d67b9b6ddd55ebd7e0
Alexis Fecteau <newbiedawannabe@gmail.com>
2025-03-25 16:36:08 -0400

    Moved filter spinners to filter popup

--- app/src/main/java/com/alexis_jimmy_yasmine/agencetouristique7/vue/activites/AccueilActivity.java
+++ app/src/main/java/com/alexis_jimmy_yasmine/agencetouristique7/vue/activites/AccueilActivity.java
@@ -24,10 +24,11 @@ import java.util.regex.Pattern;
 public class AccueilActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener, TextWatcher {
 
     private ModelView modelView;
-    private EditText searchEditText;
-    private Spinner spinBudgetAccueil, spinTypeAccueil;
+    private EditText searchEditText, popupTypeEditText, popupBudgetEditText, popupPaysEditText, popupDateEditText;
     private ListView voyagesListView;
     private ImageButton btnHome, btnHistorique, btnLogout, btnFiltre;
+
+    private PopupWindow popup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,15 +49,23 @@ public class AccueilActivity extends AppCompatActivity implements View.OnClickLi
 
         // récupérer les composantes de la vue
         searchEditText = (EditText) findViewById(R.id.searchEditText);
-        spinBudgetAccueil = (Spinner) findViewById(R.id.budgetSpinner);
-        spinTypeAccueil = (Spinner) findViewById(R.id.typeSpinner);
         voyagesListView = (ListView) findViewById(R.id.voyagesListView);
 
         searchEditText.addTextChangedListener(this);
-        spinBudgetAccueil.setOnItemSelectedListener(this);
-        spinTypeAccueil.setOnItemSelectedListener(this);
 
         voyagesListView.setOnItemClickListener(this);
+
+        //obtenir les éléments du popup de filtrage
+        LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_filtre, null);
+        popup = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+
+        popupTypeEditText = popupView.findViewById(R.id.popupTypeEditText);
+        popupBudgetEditText = popupView.findViewById(R.id.popupBudgetEditText);
+        popupPaysEditText = popupView.findViewById(R.id.popupPaysEditText);
+        popupDateEditText = popupView.findViewById(R.id.popupDateEditText);
+
+        popup.setOnDismissListener(this::filtrerVoyages);
 
         // Observer la liste des voyages
         modelView = new ViewModelProvider(this).get(ModelView.class);
@@ -93,19 +102,19 @@ public class AccueilActivity extends AppCompatActivity implements View.OnClickLi
             startActivity(intent);
 
         } else if (id == R.id.buttonFiltre) {
-            LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
-            View popupView = inflater.inflate(R.layout.popup_filtre, null);
-            PopupWindow popup = new PopupWindow(popupView, ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
             popup.showAtLocation(view, Gravity.CENTER, 0, 0);
         }
     }
 
     // Gestion du filtre
     public void filtrerVoyages() {
+
         // contruit la requete GET
         String nomLike = searchEditText.getText().toString();
-        String budget = spinBudgetAccueil.getSelectedItem().toString();
-        String type = spinTypeAccueil.getSelectedItem().toString();
+        String budget = popupBudgetEditText.getText().toString();
+        String type = popupTypeEditText.getText().toString();
+        String pays = popupPaysEditText.getText().toString();
+        String date = popupDateEditText.getText().toString();
 
         String url = "/?";
         int[] budgetRange = null;
@@ -115,7 +124,26 @@ public class AccueilActivity extends AppCompatActivity implements View.OnClickLi
         if (!nomLike.isEmpty()) {
             regex = Pattern.compile(nomLike, Pattern.CASE_INSENSITIVE);
         }
+
         // Budget
+        try{
+            int budgetNum = Integer.parseInt(budget);
+            if (budgetNum > 600){
+                budgetRange = new int[]{600, Integer.MAX_VALUE};
+            } else if (budgetNum > 300){
+                budgetRange = new int[]{300, 600};
+            } else if (budgetNum >= 0){
+                budgetRange = new int[]{0, 300};
+            } else {
+                throw new NumberFormatException("Le budget doit être positif!");
+            }
+
+        } catch (NumberFormatException e){
+            budgetRange = new int[]{0, 0};
+        }
+
+        /*
+        //TODO: revenir à cette logique commentée quand on implémentera le slider
         if (!budget.equalsIgnoreCase("Tous les budgets")) {
             switch (budget) {
                 case "Moins de 300$": budgetRange = new int[]{0, 300}; break;
@@ -123,13 +151,23 @@ public class AccueilActivity extends AppCompatActivity implements View.OnClickLi
                 case "Plus de 600$": budgetRange = new int[]{600, 2147483647}; break;
             }
         }
+        */
+
         // Type
-        if (!type.equalsIgnoreCase("Tous les types")) {
+        if (!type.isBlank()) {
             url += "type_de_voyage=" + type + "&";
         }
         url = url.substring(0, url.length() - 1);
 
+        //Pays
+        //TODO: implémenter filtrage par pays
+
+        //Date
+        //TODO: implémenter filtrage par date
+
         modelView.chargerVoyages(url, budgetRange, regex);
+
+
     }
 
     // Envoyer vers l'activité de détail
